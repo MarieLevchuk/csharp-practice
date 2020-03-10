44cf388e27c888b73c1adc4db1a5ad5bbaebc331
Мария <marie.levchuk@gmail.com>
2020-03-10 21:33:35 +0300

    added task1

---
+++ BlankSolution/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short num1 = 10;
+            object obj1 = num1;
+            sbyte num2 = (sbyte)(short)obj1;
+            Console.WriteLine(num2);
+            Console.WriteLine(num2.GetType());
+
+
+        }
+    }
+}
