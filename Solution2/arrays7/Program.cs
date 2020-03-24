19ad4f76b99b7d3551eb109fd6198ad7fc0e781e
Мария <marie.levchuk@gmail.com>
2020-03-24 20:03:47 +0300

    Added task2

--- Solution2/arrays7/Program.cs
+++ Solution2/arrays7/Program.cs
@@ -7,10 +7,31 @@ namespace arrays7
         static void Main(string[] args)
         {
             Task_1();
+            Task_2();
         }
         static void Task_1()
         {
             Console.WriteLine("Пустой массив создать невозможно");
         }        
+        static void Task_2()
+        {
+            object[] arr = new object[3];
+            arr[0] = 32;
+            arr[1] = 'a';
+            arr[2] = "Hello";
+            foreach (var item in arr)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.WriteLine();
+            
+            object obj1 = 10;
+            arr[0] = (int)arr[0]+10;
+            arr[2] += ", guys!";
+            foreach (var item in arr)
+            {
+                Console.Write($"{item}, ");
+            }        
+        }
     }
 }
