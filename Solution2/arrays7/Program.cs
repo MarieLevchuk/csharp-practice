f1f25b0a69cfeac89f812ac8aa839b56b0e5b259
Мария <marie.levchuk@gmail.com>
2020-03-24 19:53:23 +0300

    Added task1

---
+++ Solution2/arrays7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace arrays7
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            Task_1();
+        }
+        static void Task_1()
+        {
+            Console.WriteLine("Пустой массив создать невозможно");
+        }        
+    }
+}
