8bc4fee31eedb54cee2167685c3e6ed9c1b12d80
Мария <marie.levchuk@gmail.com>
2020-04-14 21:56:04 +0300

    lect_11

---
+++ BlankSolution/Lect_11.Shapes/Square.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lect_11.Shapes
+{
+    class Square : Shape
+    {
+        public Square()
+        {
+            Name = "square";
+        }
+        public override void AreaCalculate()
+        {
+            Area = Math.Pow(Side, 2);
+        }
+    }
+}
