98e38c930584c47824b25bfe7a54b8423cea24d4
Мария <marie.levchuk@gmail.com>
2020-04-23 20:12:19 +0300

    Added Lect14.Task2

---
+++ BlankSolution/lect14.Task2/Neighbor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lect14.Task2
+{
+    class Neighbor
+    {
+        public string LastName { get; set; }
+        public int Flat { get; set; }
+        public long PhoneNumber { get; set; }     
+    }
+}
